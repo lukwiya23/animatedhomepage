e31d33795cb48021130e93932889c1ca1f6ca3df
lukwiya23 <luk23bonnie8@gmail.com>
2020-03-31 18:07:53 +0300

    changing ui

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-   const hero = document.querySelector(".hero")
-   const slider = document.querySelector(".slider")
-   const logo = document.querySelector("#logo")
-   const hamburger = document.querySelector(".hamburger")
-   const headline = document.querySelector(".headline")
+const tl = gsap.timeline()
+tl.fromTo(".hero", {
+    height: "0%"
+}, {
+    height: "80%",
+    duration: 1,
+    ease: Power2.easeInOut
+})
+tl.fromTo(".hero", { width: "100%" }, {
+    width: "80%",
+    duration: 1.2,
+    ease: Power2.easeInOut
 
-   const tl = gsap.timeline()
-   tl.fromTo(".hero", {
-       height: "0%"
-   }, {
-       height: "80%",
-       duration: 1,
-       ease: Power2.easeInOut
-   })
-   tl.fromTo(".hero", { width: "100%" }, {
-       width: "80%",
-       duration: 1.5,
-       ease: Power2.easeInOut
+})
+tl.fromTo(".slider", {
+    x: "-100%",
 
-   })
+}, {
+    x: "0%",
+    ease: Power1.easeInOut,
+    duration: 1.2
+
+})
